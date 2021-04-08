fd415c0546e77b9fe09b1d569fdbcef62c16edcb
AL AMIN <aminspicse@gmail.com>
2021-04-08 19:55:34 +0600

    First 3's a Last 3's b

--- first3aaalast3bbb.cpp
+++ first3aaalast3bbb.cpp
@@ -1,3 +1,9 @@
+/*
+    Write a Cpp program to recognize strings starts with 3 a's and ends with 3 b's.
+    (Solve the problem in code blocks, take a screenshot of the code portion and attach
+     that in the below attachment section.)  *
+*/
+
 #include <iostream>
 #include <string>
 
